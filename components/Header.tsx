f4a7b26e68fa1813593d530db7efde4abb198dbd
sifuna2002 <sifunadone4704@gmail.com>
2022-09-28 13:19:48 +0300

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {SocialIcon} from 'react-social-icons'
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+        <div className="flex flex-row items-center"> 
+            <SocialIcon 
+                url="https://www.youtube.com/sifunadonex"
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <SocialIcon 
+                url="https://www.youtube.com/sifunadonex"
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <SocialIcon 
+                url="https://www.youtube.com/sifunadonex"
+                fgColor='gray'
+                bgColor='transparent'
+            />
+        </div> 
+        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+            <SocialIcon 
+                className='cursor-pointer'
+                network='email'
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+        </div>
+    </header>
+  )
+}
