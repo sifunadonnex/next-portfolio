fdfccd0a560ddd07d6fbc86c0c0622ddd635af01
sifuna2002 <sifunadone4704@gmail.com>
2022-09-28 13:46:59 +0300

    header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import {SocialIcon} from 'react-social-icons'
+import {motion} from 'framer-motion'
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-        <div className="flex flex-row items-center"> 
+        <motion.div
+        initial={{
+            opacity: 0,
+            x:-500,
+            scale: 0.5,
+        }}
+        animate={{
+            opacity: 1,
+            x:0,
+            scale: 1,
+        }}
+        transition={{
+            duration: 1.5,
+        }}
+         className="flex flex-row items-center"> 
             <SocialIcon 
                 url="https://www.youtube.com/sifunadonex"
                 fgColor='gray'
@@ -21,8 +36,22 @@ export default function Header({}: Props) {
                 fgColor='gray'
                 bgColor='transparent'
             />
-        </div> 
-        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        </motion.div> 
+        <motion.div
+        initial={{
+            opacity: 0,
+            x:500,
+            scale: 0.5,
+        }}
+        animate={{
+            opacity: 1,
+            x:0,
+            scale: 1,
+        }}
+        transition={{
+            duration: 1.5,
+        }}
+         className='flex flex-row items-center text-gray-300 cursor-pointer'>
             <SocialIcon 
                 className='cursor-pointer'
                 network='email'
@@ -30,7 +59,7 @@ export default function Header({}: Props) {
                 bgColor='transparent'
             />
             <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
-        </div>
+        </motion.div>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
       <Head>
         <title>Sifuna Portfolio</title>
       </Head>
-      
       <Header />
+      <Hero />
     </div>
   )
 }
